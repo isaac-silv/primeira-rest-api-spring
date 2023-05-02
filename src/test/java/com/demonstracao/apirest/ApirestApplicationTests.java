3e2296e81fdd71667c4d236a7ba3765139be00b0
isaac-silv <isaacaquino.dv@gmail.com>
2023-05-02 17:44:04 -0300

    first commit

---
+++ src/test/java/com/demonstracao/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demonstracao.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
